4ea3fcf9608bb6a1bb5692f77ef3d49aeab3d5a4
EsaveraAdmin <easvera2025@gmail.com>
2025-08-30 02:33:09 +0530

    Fixing home page issue

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home: React.FC = () => {
     <div>
       <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem 0' }}>
         <img
-          src="esavera.png"
+          src={`${import.meta.env.BASE_URL}esavera.png`}
           alt="Esavera Solution Logo"
           style={{ width: '150px', height: 'auto' }} // Adjust size as you wish
         />
